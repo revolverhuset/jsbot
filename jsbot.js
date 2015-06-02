dc98fb8a357777d525eb56d3517f839f8c2b8a4b
Jon Packer <contact@jonpacker.com>
2015-06-02 16:09:08 +0200

    omg jsbot

--- jsbot.js
+++ jsbot.js
@@ -11,35 +11,16 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+process.stdout.on('data', function(data) {
+  request.post(hookUrl, { form: JSON.stringify({ text: data }) }, function(err, resp, body) { });
+});
+process.stderr.on('data', function(data) {
+  request.post(hookUrl, { form: JSON.stringify({ text: data }) }, function(err, resp, body) { });
+});
+
 app.post('/', function(req, res) {
-  var usage = "Valid commands: " + Object.keys(randyCommands).join(", ");
-
-  if (!req.body.text)
-    return res.status(400).send(usage);
-
-  var user = req.body.user_name || "<hax0r>";
-  var text = req.body.text;
-  // shortcut to d6 etc...
-  if (/^d\d/.test(text))
-    text = text.replace(/d/, "d ");
-
-  var args = text.split(" ");
-  var func = args.shift();
-  if (!randyCommands[func])
-    return res.status(400).send(usage);
-  try {
-    var result = randyCommands[func](args);
-    var chatMessage = user + ": " + req.body.text + '\n> ' + result;
-    request.post(hookUrl, { form: JSON.stringify({ text: chatMessage }) }, function(err, resp, body) {
-      if (err)
-        return res.status(500).send(err);
-      res.status(200).end();
-    });
-  } catch(e) {
-    res.status(500).send(e.message || "I'm confused.");
-  }
+  if (!req.body.text) return res.send(400, 'undefined');
+  console.log(eval(req.body.text));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), function() { });

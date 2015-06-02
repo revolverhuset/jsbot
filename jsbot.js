133c1529de9437c39e5b0cc4a19be09a0292b128
Jon Packer <contact@jonpacker.com>
2015-06-02 16:46:43 +0200

    dont ever do this

--- jsbot.js
+++ jsbot.js
@@ -10,12 +10,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-process.stdout.on('data', function(data) {
+process.stdout.write = process.stderr.write = function(data) {
   request.post(hookUrl, { form: JSON.stringify({ text: data }) }, function(err, resp, body) { });
-});
-process.stderr.on('data', function(data) {
-  request.post(hookUrl, { form: JSON.stringify({ text: data }) }, function(err, resp, body) { });
-});
+};
 
 app.post('/', function(req, res) {
   if (!req.body.text) return res.send(400, 'undefined');
